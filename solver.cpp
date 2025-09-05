6cf45e9193c5442df6edffb1934fd9a5e8972059
KUNAL DHYANI <118094003+KunalDhyani22@users.noreply.github.com>
2025-09-05 17:24:12 +0530

    Update solver.cpp

--- solver.cpp
+++ solver.cpp
@@ -11,6 +11,80 @@ struct State{
     State(int n): helicopterVillage(n){};
 };
 
+double calculateObjectiveValue(const Solution& solution, ProblemData& problem)
+{
+    double totalValue, totalCost;
+    totalCost=totalValue=0;
+
+    int H = (int)problem.helicopters.size();
+    int V = (int)problem.villages.size();
+
+    vector<double> foodDelivered(V+1, 0.0);
+    vector<double> otherDelivered(V+1, 0.0);
+    vector<double> helicopterTotalDistance(H, 0.0);
+
+    for(int h = 0;h<H; h++)
+    {
+        const auto& plan = solution[h];
+        const auto& helicopter = problem.helicopters[plan.helicopter_id-1];
+
+        if(plan.trips.empty())
+        continue;
+
+        totalCost += helicopter.fixed_cost;
+
+        for(const auto& trip: plan.trips)
+        {
+            if(trip.drops.empty())
+            continue;
+
+            double tripWeight = trip.dry_food_pickup*problem.packages[0].weight;
+            tripWeight += trip.perishable_food_pickup*problem.packages[1].weight;
+            tripWeight += trip.other_supplies_pickup*problem.packages[2].weight;
+
+            if(tripWeight > helicopter.weight_capacity)
+            return -1e9;
+            int totalDryDrops = 0, totalPerishableDrops = 0, totalOtherDrops = 0;
+
+            for(const auto& drop : trip.drops)
+            {
+                totalDryDrops += drop.dry_food;
+                totalOtherDrops += drop.other_supplies;
+                totalPerishableDrops += drop.perishable_food;
+            }
+
+            if (totalDryDrops > trip.dry_food_pickup ||
+                totalPerishableDrops > trip.perishable_food_pickup ||
+                totalOtherDrops > trip.other_supplies_pickup)
+                return -1e9;
+
+            vector<int> route;
+            for(const auto& drop: trip.drops)
+            route.push_back(drop.village_id);
+
+            double tripDistance = calculateTourDistance(h, route, problem);
+            if(tripDistance > helicopter.distance_capacity)
+            return -1e9;
+
+            if(helicopterTotalDistance[h] > problem.d_max - tripDistance)
+            return -1e9;
+
+            helicopterTotalDistance[h] += tripDistance;
+
+            for(const auto& drop: trip.drops)
+            {
+                totalValue += calculateVillageValue(drop.village_id, drop.dry_food, drop.perishable_food, drop.other_supplies, foodDelivered[drop.village_id], otherDelivered[drop.village_id]);
+                foodDelivered[drop.village_id] += drop.dry_food + drop.perishable_food;
+                otherDelivered[drop.village_id] += drop.other_supplies;
+            }
+            
+            totalCost += helicopter.alpha*tripDistance;
+        }
+
+    }
+    return totalValue - totalCost;
+}
+
 State Random_State(const ProblemData& problem){
     random_device rd;
     mt19937 rng(rd());
