d1b9d052ef185e001a5155dca69aec30cea5bc17
Priyanshu <110831618+Priyanshu0Saxena@users.noreply.github.com>
2025-09-05 15:59:56 +0530

    Added Random_State() function
    each village is added in a randomly selected helicopter

--- solver.cpp
+++ solver.cpp
@@ -1,6 +1,7 @@
 #include "solver.h"
 #include <iostream>
 #include <chrono>
+#include <random>
 
 using namespace std;
 
@@ -9,6 +10,22 @@ struct State{
     State(){}
     State(int n): helicopterVillage(n){};
 };
+
+State Random_State(const ProblemData& problem){
+    random_device rd;
+    mt19937 rng(rd());
+    int H=problem.helicopters.size();
+    int V=problem.villages.size();
+    State state;
+    state.helicopterVillage.resize(H);
+    int target_Helicopter;
+    uniform_int_distribution<int>dist(0,H-1);
+    for(int i=0;i<V;i++){
+        target_Helicopter=dist(rng);
+        state.helicopterVillage[target_Helicopter].push_back(problem.villages[i].id);
+    }
+    return state;
+}
 
 Solution solve(const ProblemData& problem) {
     cout << "Starting solver..." << endl;
